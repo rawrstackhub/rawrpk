40968c2b0874a0ed6dfc4054240d614cde0939b5
CorpoWolf <gmangavin1@gmail.com>
2024-03-25 12:54:31 -0700

    Added JSON parsing for github

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
 )
+
+type RepoFile struct {
+	Name string `json:"name"`
+	Path string `json:"path"`
+	URL  string `json:"download_url"`
+}
 
 func main() {
 	flag.Parse()
@@ -40,10 +49,47 @@ func handleGetCommand(sourceIdentifier string) {
 	switch source {
 	case "github":
 		fmt.Printf("Fetching from GitHub: %s\n", identifier)
+		repository := strings.Split(identifier, "/")
+		ParseGit(repository)
 	case "url":
 		fmt.Printf("Fetching from URL: %s\n", identifier)
 	default:
 		fmt.Printf("Unsupported source: %s\n", source)
 		os.Exit(1)
+	}
+}
+
+func ParseGit(repo []string) {
+	fmt.Println("Parsing page:", repo)
+
+	// GitHub API endpoint to list repository contents
+	apiURL := fmt.Sprintf("https://api.github.com/repos/%s/%s/contents/", repo[0], repo[1])
+
+	// Make an HTTP GET request
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		fmt.Printf("Error fetching repository contents: %s\n", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Read and parse the JSON response
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error reading response body: %s\n", err)
+		return
+	}
+
+	var files []RepoFile
+	if err := json.Unmarshal(body, &files); err != nil {
+		fmt.Printf("Error parsing JSON: %s\n", err)
+		return
+	}
+
+	// Look for files ending with .rawrpk
+	for _, file := range files {
+		if strings.HasSuffix(file.Name, ".rawrpk") {
+			fmt.Printf("Found .rawrpk file: %s, Download URL: %s\n", file.Name, file.URL)
+		}
 	}
 }
